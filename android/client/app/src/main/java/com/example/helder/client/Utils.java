45b82ab0bb13f62130a3d8695dd46704eee0c22b
Nelson Rodrigues <nelsonrds95@gmail.com>
2017-04-13 15:47:34 +0100

    Checkbox in listview returns the object

--- android/client/app/src/main/java/com/example/helder/client/Utils.java
+++ android/client/app/src/main/java/com/example/helder/client/Utils.java
@@ -11,4 +11,5 @@ public class Utils {
     public static final String param_sharedPreferences = "sharedpreferences";
     public static final String param_username = "username";
     public static final String param_password = "password";
+    public static final String URL_PRINCIPAL = "http://5a5c4e3e.ngrok.io";
 }
