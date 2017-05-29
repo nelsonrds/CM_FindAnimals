359f6d43d2fa281148f3260cbb2924fafcee27c3
DocAdmin <docadmin@Lab3711.local>
2017-05-29 11:36:16 +0100

    First commit

---
+++ ios/Animal Track/Animal Track/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal Track
+//
+//  Created by DocAdmin on 5/29/17.
+//  Copyright © 2017 DocAdmin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios/Animal Track/Animal TrackTests/Animal_TrackTests.swift
@@ -0,0 +1,36 @@
+//
+//  Animal_TrackTests.swift
+//  Animal TrackTests
+//
+//  Created by DocAdmin on 5/29/17.
+//  Copyright © 2017 DocAdmin. All rights reserved.
+//
+
+import XCTest
+@testable import Animal_Track
+
+class Animal_TrackTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
